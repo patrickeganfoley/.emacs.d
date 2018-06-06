4d76fc159b8e4a508cc854d98e9bfa52323a4b7e
Patrick <patrick.egan.foley@gmail.com>
2018-06-06 13:47:01 -0700

    Add custom.el
    New laptop.  Checked, and there is nothing dangerous in this.  It
    looks like just a record of set options.  Don't like it though.

---
+++ custom.el
@@ -0,0 +1,49 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(beacon-color "#ff9da4")
+ '(custom-safe-themes
+   (quote
+    ("599f1561d84229e02807c952919cd9b0fbaa97ace123851df84806b067666332" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "53f97243218e8be82ba035ae34c024fd2d2e4de29dc6923e026d5580c77ff702" "2887f73fc0a89beaa18688aad1ad019505c0b12aeba5137edb16cb03f32fbc8b" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "243bd9824b2a2203c4cf22e306e4fba73f9e6d6f0b032176876980da471bdca5" default)))
+ '(ensime-sem-high-faces
+   (quote
+    ((var :foreground "#9876aa" :underline
+	  (:style wave :color "yellow"))
+     (val :foreground "#9876aa")
+     (varField :slant italic)
+     (valField :foreground "#9876aa" :slant italic)
+     (functionCall :foreground "#a9b7c6")
+     (implicitConversion :underline
+			 (:color "#808080"))
+     (implicitParams :underline
+		     (:color "#808080"))
+     (operator :foreground "#cc7832")
+     (param :foreground "#a9b7c6")
+     (class :foreground "#4e807d")
+     (trait :foreground "#4e807d" :slant italic)
+     (object :foreground "#6897bb" :slant italic)
+     (package :foreground "#cc7832")
+     (deprecated :strike-through "#a9b7c6"))))
+ '(epg-gpg-program "gpg1")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(flycheck-python-pycompile-executable "python2")
+ '(frame-background-mode (quote dark))
+ '(fringe-mode 10 nil (fringe))
+ '(linum-format " %6d ")
+ '(main-line-color1 "#222232")
+ '(main-line-color2 "#333343")
+ '(package-selected-packages
+   (quote
+    (flycheck-mypy vmd-mode projectile rainbow-delimiters ess company multiple-cursors go-mode scala-mode org-mode osx-browse exec-path-from-shell restclient slack zenburn-theme zenburn yaml-mode white-sand-theme use-package smex smartparens py-yapf polymode monokai-theme markdown-mode magit latex-preview-pane latex-pretty-symbols latex-math-preview google-this github-theme gh-md flymake-yaml flycheck ess-smart-equals emacsql-psql ein darcula-theme danneskjold-theme cycle-themes company-jedi color-theme-zenburn color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-theme-github clues-theme browse-at-remote)))
+ '(powerline-color1 "#222232")
+ '(powerline-color2 "#333343"))
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
