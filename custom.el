956ba293f6b1c9db6518485c11bc21e810af40e4
Patrick Foley <patrick.foley@mbp16-64-1000-4952p.lan>
2022-05-12 19:36:10 -0700

    Will remove custom.el.
    You don't actually want this on startup for a new machine

--- custom.el
+++ custom.el
@@ -3,18 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(compilation-message-face 'default)
  '(custom-safe-themes
-   '("41c8c11f649ba2832347fe16fe85cf66dafe5213ff4d659182e25378f9cfc183" "a63355b90843b228925ce8b96f88c587087c3ee4f428838716505fd01cf741c8" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" "599f1561d84229e02807c952919cd9b0fbaa97ace123851df84806b067666332" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "53f97243218e8be82ba035ae34c024fd2d2e4de29dc6923e026d5580c77ff702" "2887f73fc0a89beaa18688aad1ad019505c0b12aeba5137edb16cb03f32fbc8b" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "243bd9824b2a2203c4cf22e306e4fba73f9e6d6f0b032176876980da471bdca5" default))
- '(debug-on-error t)
- '(linum-format " %4d ")
- '(package-selected-packages
-   '(org-bullets doom-themes json-mode ein elpy intellij-theme blacken forge ess-R-object-popup ess-site flycheck-mypy ess company org-mode osx-browse exec-path-from-shell slack zenburn-theme zenburn white-sand-theme smex smartparens py-yapf latex-preview-pane latex-pretty-symbols google-this github-theme gh-md flymake-yaml flycheck ess-smart-equals emacsql-psql darcula-theme danneskjold-theme cycle-themes company-jedi color-theme-zenburn color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-theme-github clues-theme browse-at-remote))
-
+   '("e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-)
